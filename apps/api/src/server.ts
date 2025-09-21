0c0bcf5ee323fd8628d815bfaa2dc83a3b55e852
Ekam <bhat0155@algonauinlive.com>
2025-09-21 18:12:15 -0400

    removed console

---
+++ apps/api/src/server.ts
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+// loads env
+dotenv.config();
+const app = express();
+app.use(helmet());  // security headers
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+
+app.use(express.json()); // for parsing application/json
+app.use(morgan("dev")); // logging
+
+// creating a health route
+app.get("/health", (req, res)=>{
+    res.json({ok: true})
+})
+
+const PORT = process.env.PORT || 4000;
+
+
+app.listen(PORT, ()=>{
+    console.log("API server is running on port", PORT);
+})
